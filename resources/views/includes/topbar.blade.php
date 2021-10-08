3ae209c05531350aa88c143992e487aa039f7752
Gabriel <gabrielodadier@gmail.com>
2021-10-08 20:33:13 +0000

    Edited Top Bar.

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -11,12 +11,6 @@
         </div>
         <div class="col-md-5">
             <div class="row justify-content-end">
-                <div class="mr-3">
-                    <a href="#" class="btn btn-light bg-white btn-lg py-1 px-2" style="border-radius: 40%"><span class="text-success"><i class="fa fa-comments"></i></span></a>
-                </div>
-                <div class="mr-4">
-                    <a href="#" class="btn btn-light bg-white btn-lg py-1 px-2" style="border-radius: 40%"><span class="text-success"><i class="fa fa-bell"></i></span></a>
-                </div>
                 <div class="mr-4">
                     <img src="{{asset('img/profile.jpg')}}" class="img img-fluid border border-success" style="border-radius: 50%; width: 45px; height: 45px;"/>
                 </div>
